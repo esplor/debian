2eec940e31acd7b201de48d1ad542eb640b250f1
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2023-10-01 22:40:29 +0200

    Update qtile script

--- debian-qtile.sh
+++ debian-qtile.sh
@@ -1,12 +1,25 @@
-sudo apt-get install git build-essential python3-venv \
-	libxkbcommon-dev lightdm lm-sensors alacritty \
-	stow locales bat
+SHELL='/usr/bin/env sh'
+
+sudo $SHELL -c 'cat <<EOF> /etc/apt/sources.list
+deb http://deb.debian.org/debian                bookworm                        main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian                bookworm-updates                main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian                bookworm-backports              main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian-security/      bookworm-security               main contrib non-free non-free-firmware
+
+# *** Optional ***
+#deb http://deb.debian.org/debian                bookworm-backports-sloppy       main contrib non-free non-free-firmware
+#deb http://deb.debian.org/debian                bookworm-proposed-updates       main contrib non-free non-free-firmware
+EOF'
+
+sudo apt-get update && sudo apt-get -y install git build-essential \
+	python3-venv libxkbcommon-dev lightdm lm-sensors alacritty stow \
+	locales bat
 
 sudo python3 -m venv /opt/qtile --upgrade-deps
 sudo /opt/qtile/bin/pip install --no-cache-dir cairocffi
 sudo /opt/qtile/bin/pip install qtile
 
-sudo bash -c 'cat <<EOF> /usr/share/xsessions/qtile.desktop
+sudo $SHELL -c 'cat <<EOF> /usr/share/xsessions/qtile.desktop
 [Desktop Entry]
 Name=Qtile
 Comment=Qtile session
