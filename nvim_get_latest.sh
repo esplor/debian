8eaf13513b51f2c67e6df0377b26c6ffc4c3f1ba
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2023-10-10 21:21:11 +0200

    Minor change

--- nvim_get_latest.sh
+++ nvim_get_latest.sh
@@ -8,4 +8,4 @@ mkdir -p $dir
 fi
 
 wget -qO- https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz | tar xvz -C $dir --strip-components=1
-echo '-----\n'$dir'/bin needs to be included in path, export PATH='$dir'/bin:$PATH\n-----'
+echo '-----\n'$dir'/bin needs to be included in path... add this to .bashrc or .zshrc\nexport PATH='$dir'/bin:$PATH\n-----'
