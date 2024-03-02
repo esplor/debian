ca973b30d16b66f1b5a72db1db878da4654056da
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-03-02 17:46:33 +0100

    MC

--- virtualbox_install.sh
+++ virtualbox_install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 
 echo '\n\t*** Done ***\n\tRemember to add your user to vboxusers group:\n\tsudo usermod' $(whoami) '-aG vboxusers\n\n'
