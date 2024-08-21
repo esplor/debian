073b5f1ccbe530ac02d4d0032ae4d6647c06b28a
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-08-21 23:33:39 +0200

    Do not prompt for allow software insstallation

--- flatpak_install.sh
+++ flatpak_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 sudo apt-get update
-sudo apt-get install flatpak gnome-software-plugin-flatpak
+sudo apt-get -y install flatpak gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
