73414ac4ce05a23dac5cfa9b397963c6d092115d
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-08-31 23:34:14 +0200

    Cleanup virtualbox install script

--- virtualbox_install.sh
+++ virtualbox_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | gpg --dearmor | sudo tee /usr/share/keyrings/oracle-virtualbox.gpg > /dev/null
 
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox.gpg] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/oracle-virtualbox.list > /dev/null
 
 echo '\n\t*** Done ***\n\tRemember to add your user to vboxusers group:\n\tsudo usermod' $(whoami) '-aG vboxusers\n\n'
