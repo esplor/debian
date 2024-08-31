e8eeac13447edcbcaa023c40743db7e27c3da01d
Espen Lorentzen <3808473+esplor@users.noreply.github.com>
2024-08-31 23:33:21 +0200

    Add bat and build-essential

--- base_pkgs_install.sh
+++ base_pkgs_install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-sudo apt-get -y install bash-completion stow htop locales
+sudo apt-get -y install bash-completion bat build-essential htop locales stow
 sudo dpkg-reconfigure locales
 sudo dpkg-reconfigure console-setup
